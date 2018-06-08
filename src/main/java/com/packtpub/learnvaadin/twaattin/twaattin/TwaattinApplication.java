d9c3e3822b33e6f588bffa34d9a46e4a068873c8
nenad <nenad.nenadovic@kapsch.net>
2018-06-08 22:51:17 +0200

    initial commit

---
+++ src/main/java/com/packtpub/learnvaadin/twaattin/twaattin/TwaattinApplication.java
@@ -0,0 +1,12 @@
+package com.packtpub.learnvaadin.twaattin.twaattin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwaattinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwaattinApplication.class, args);
+	}
+}
